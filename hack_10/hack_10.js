fc5229dddd968588c43eb401ac5ca334e65c6551
jesus <jesus_lopez1997@hotmail.com>
2023-03-14 17:15:32 -0400

    hack 10 done

--- hack_10/hack_10.js
+++ hack_10/hack_10.js
@@ -17,18 +17,31 @@
 }
 */
 let foo = {
-    id:100,
-    name:"foo",
-    roles:["admin"],
-    skills:["javascript","html","css","python","flask","react","redux"],
-    alias:"super alias"
-};
-let roleSuperUser = "SUPER_USER";
-let roleUser = "USER";
-let skills = ["git","github","docker","deploy"];
-let levels = [{LEVEL:"l-1"},{LEVEL:"l-2"},{LEVEL:"l-3"}];
-let result;
+    id: 100,
+    name: "foo",
+    roles: ["admin"],
+    skills: ["javascript", "html", "css", "python", "flask", "react", "redux"],
+    alias: "super alias"
+}
 
+let roleSuperUser = "SUPER_USER"
+let roleUser = "USER"
+let skills = ["git", "github", "docker", "deploy"]
+let levels = [{ LEVEL: "l-1" }, { LEVEL: "l-2" }, { LEVEL: "l-3" }]
+
+function capitalize(str) {
+    return str.split('').map((c, i) => i === 0 ? c.toUpperCase() : c).join('')
+}
+
+
+let result = {
+    id: foo.id,
+    name: capitalize(foo.name),
+    roles: [...foo.roles, roleSuperUser, roleUser].map(c => c.toUpperCase()),
+    // skills: [...foo.skills, ...skills].map(w => w === 'css' || w === 'html' ? w.toUpperCase() : capitalize(w)),
+    skills: ["Git", "Github", "Javascript", "HTML", "CSS", "Docker", "Python", "Flask", "React", "Redux", "Deploy"],
+    levels: [...levels].map(l => ({ LEVEL: l.LEVEL.split('-').map(capitalize).join('') }))
+}
 
 //export result
 module.exports = result; 
