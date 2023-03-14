10a1f9c09ab7afb48d30d5b55cd42d5a80b8a159
jesus <jesus_lopez1997@hotmail.com>
2023-03-14 14:01:15 -0400

    hack 6 done

--- hack_6/hack_6.js
+++ hack_6/hack_6.js
@@ -16,6 +16,12 @@ let result = {
     role:"admin"
 };
 
+function capitalize(str) {
+    return str.split('').map((c, i) => i === 0 ? c.toUpperCase() : c).join('')
+}
+
+result.name = capitalize(result.name)
+result.role = result.role.toUpperCase()
  
 //export result
 module.exports = result; 
