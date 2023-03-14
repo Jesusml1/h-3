83760cf8050d27996b30805278da37bff5a40883
jesus <jesus_lopez1997@hotmail.com>
2023-03-14 13:10:09 -0400

    hack 2 and 3 done

--- hack_2/hack_2.js
+++ hack_2/hack_2.js
@@ -6,9 +6,16 @@
 * 
 * output => [2,3,4]
 */
-let arr = [{a:1},{b:2},{c:3},{d:4},{e:5}];
-let result = [];
- 
- 
+let arr = [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }, { e: 5 }]
+let result = []
+
+for (let i = 0; i < arr.length; i++) {
+    if (i === 0 || i === arr.length - 1) {
+        continue
+    } 
+
+    Object.entries(arr[i]).map((e) => result.push(e[1]))
+}
+
 //export result
-module.exports = result;
+module.exports = result
--- hack_3/hack_3.js
+++ hack_3/hack_3.js
@@ -9,6 +9,12 @@
 let arr = [{a:1},{b:2},{c:3},{d:4},{e:5}];
 let result = [];
  
+for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 0) {
+        Object.entries(arr[i]).map((e) => result.push(e[1]))
+    } 
+
+}
  
 //export result
 module.exports = result; 
