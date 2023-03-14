fb0ab11c918395e474ac693a7f7b4d4d4d6b39d4
jesus <jesus_lopez1997@hotmail.com>
2023-03-14 12:51:45 -0400

    hack 1 done and update gitignore

--- hack_1/hack_1_for.js
+++ hack_1/hack_1_for.js
@@ -6,9 +6,12 @@
  * 
  * output => [1,2,3,4,5]
  */
-let arr = [{a:1},{b:2},{c:3},{d:4},{e:5}];
-let result = [];
+let arr = [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }, { e: 5 }]
+let result = []
 
+for (let o of arr) {
+    Object.entries(o).map((e) => result.push(e[1]))
+}
 
 //export result
-module.exports = result;
+module.exports = result
--- hack_1/hack_1_while.js
+++ hack_1/hack_1_while.js
@@ -6,9 +6,16 @@
 * 
 * output => [1,2,3,4,5]
 */
-let arr = [{a:1},{b:2},{c:3},{d:4},{e:5}];
-let result = [];
- 
- 
+let arr = [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }, { e: 5 }]
+let result = []
+let i = 0
+
+while (i < arr.length) {
+    Object.entries(arr[i]).map((e) => result.push(e[1]))
+    i++
+}
+console.log(result)
+
+
 //export result
-module.exports = result;
+module.exports = result
